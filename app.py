cd32ad93a4969b3f1fb58dbbd7e2f261761fd89d
jzettac <jczetta@gmail.com>
2022-03-19 23:18:15 -0400

    Add better error handling

--- app.py
+++ app.py
@@ -290,8 +290,20 @@ def index():
 
 # Error handling
 
-# TODO
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
+@app.errorhandler(403)
+def permission_denied(e):
+    # note that we set the status explicitly
+    return render_template('403.html'), 403
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    # note that we set the status explicitly
+    return render_template('500.html'), 500
 
 # Main
 
